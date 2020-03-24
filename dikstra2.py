f603202ebf39e21e2337346e430cf062a6d96fa8
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-03-23 20:00:21 -0400

    displaying path.

--- dikstra2.py
+++ dikstra2.py
@@ -1,6 +1,7 @@
 from MinHeap import MinHeap
 from graphNode2 import graphNode2
 import math
+import copy
 
 class dikstra:
 
@@ -13,6 +14,7 @@ class dikstra:
         #     [x, x, x]
         # ]
         # self.cost = [x, x, x]
+        # self.paths = [ [], [], [] ]
 
         self.el = [
            # a   b   c   d   e   f   g (edge to go to letter)
@@ -25,18 +27,28 @@ class dikstra:
             [x,  x,  x,  10, 5,  3,  0]  # g
         ]
         self.cost = [x, x, x, x, x, x, x]
+        self.paths = [ [], [], [], [], [], [], [] ]
+        
 
         self.alreadyVisited = set()
         self.toVisit = MinHeap()
 
+    def pprint(self, s):
 
+        print("Node     Effort   Path")
+        for x in range(0, len(self.cost)):
+            if x is not s:
+                print(str(x) + "          " + str(self.cost[x]) + "       " +  str(self.paths[x]))
+            else:
+                print(str(x) + "          " + str(self.cost[x]) + "       " +  str(self.paths[x]) + "<----- This is the source")
 
 
     def solve(self, s):
 
         source = graphNode2()
-        # source.setWeight(0)
-        source.setNum(0)
+        source.setNum(s)
+        # initialize the path to just itself
+        self.paths[s] = [s]
         # set the source to 0
         self.cost[s] = 0
         # add source to the places to visit
@@ -52,6 +64,14 @@ class dikstra:
                 if self.cost[curr.getNum()] + self.el[curr.getNum()][x] < self.cost[x]:
                     # set the new cost
                     self.cost[x] = self.cost[curr.getNum()] + self.el[curr.getNum()][x]
+
+                    # Erase old path, if there was one
+                    self.paths[x] = []
+                    # copy the path of least cost 
+                    self.paths[x] = copy.deepcopy(self.paths[curr.getNum()])
+                    # and append new node
+                    self.paths[x].append(x)
+                    
                     # add node toVist Set
                     temp = graphNode2()
                     temp.setNum(x)
@@ -59,11 +79,18 @@ class dikstra:
                     self.toVisit.insert(temp)
 
                     
-        print(self.cost)
+        self.pprint(s)
         #return cost matrix/print
 
 
 if __name__ == "__main__":
-    d = dikstra()
-    d.solve(0)
+
+    # d = dikstra()
+    # d.solve(0)
+
+
+    for i in range(0,7):
+        d = dikstra()
+        d.solve(i)
+
     pass
--- graphNode2.py
+++ graphNode2.py
@@ -4,12 +4,23 @@ class graphNode2:
     def __init__(self):
         self.num  = 0 
         self.weight = math.inf
+        self.path = []
 
     def setNum(self, x):
         self.num = x
     
     def getNum(self):
         return self.num
+
+    # I don't think we will need this
+    # def setPath(self, givenPath):
+    #     # copy given path
+    #     self.path = givenPath
+    #     # add yourself to the list  
+    #     self.path.append(self.getNum())
+
+    # def erasePath(self):
+    #     self.path = []
 
     def setWeight(self, x):
         self.weight = x
@@ -44,7 +55,7 @@ if __name__ == "__main__":
     x.setWeight(5)
     y = graphNode2()
     y.setWeight(50)
+    # print(y < x)
     
-    print(y < x)
     
     pass
